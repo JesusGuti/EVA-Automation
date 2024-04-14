2e5dd01ba6d616c3252ce78a35da35861f62d78b
JesusGuti <jesusgonzales0968@gmail.com>
2024-04-14 01:05:52 -0400

    Test 3 - Tipo Evento terminado

--- tests/TipoDeEvento/tipoEvento.spec.js
+++ tests/TipoDeEvento/tipoEvento.spec.js
@@ -2,31 +2,43 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({page}) => {
-    await page.goto('http://127.0.0.1:8000/');
+    await page.goto('');
     /** Aqui hacemos click en el menu lateral, para entrar en el menu **/
     const botonMenu = await page.getByRole('button', { name: 'TIPO DE EVENTO' })
     await botonMenu.click(); 
-});
-
-test("test1", async ({ page }) => {
     const opcionCrearTipoEvento = await page.getByRole('link', { name: 'Crear tipo de evento' });
     await opcionCrearTipoEvento.click();
+});
+
+test("Verificar que el nombre de tipo de evento sea una cadena de caracteres", async ({ page }) => {
     const inputNombre = await page.getByPlaceholder('Ingrese el nombre del tipo de evento');
     await inputNombre.click();
-    await inputNombre.fill("Dorian lagarto");
+    await inputNombre.fill("Competencia");
     const inputDescripcion =  await page.getByPlaceholder('Ingrese una descripción...');
-    await inputDescripcion.fill("Nueva descripcion")
+    const descripcion = "Una descripcion"
+    await inputDescripcion.fill(descripcion);
     const botonEnviar = await page.getByRole('button', { name: 'Crear' });
     await botonEnviar.click();
     await page.waitForTimeout(5000);
 });
 
-test("test2", async ({ page }) => {
-    const opcionCrearTipoEvento = await page.getByRole('link', { name: 'Crear tipo de evento' });
-    await opcionCrearTipoEvento.click();
+test("Verificar que el nombre de tipo de evento no sea nulo", async ({ page }) => {
     const botonEnviar = await page.getByRole('button', { name: 'Crear' });
     await botonEnviar.click();
     const mensajeFaltaNombre = await page.getByText("El nombre no puede estar vacío.");
     await expect(mensajeFaltaNombre).toBeVisible();
     await page.waitForTimeout(5000);
+});
+
+test("Verificar que el nombre de tipo de evento sea único", async ({ page }) => {
+    const inputNombre = await page.getByPlaceholder('Ingrese el nombre del tipo de evento');
+    await inputNombre.click();
+    await inputNombre.fill("Competencia");
+    await page.waitForTimeout(2000);
+    const botonEnviar = await page.getByRole('button', { name: 'Crear' });
+    await botonEnviar.click();
+    const alert = await page.waitForSelector('.alert');
+    const alertText = await alert.textContent();
+    await expect(alertText).toContain("El tipo de evento ya existe");
+    await page.waitForTimeout(1000);
 });
