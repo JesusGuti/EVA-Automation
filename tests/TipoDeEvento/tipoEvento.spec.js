b3b1e6db23d24852d3a5431a8f8eb14a9ff45957
JesusGuti <jesusgonzales0968@gmail.com>
2024-04-13 22:20:00 -0400

    Tipo de evento con hook beforeEach

--- tests/TipoDeEvento/tipoEvento.spec.js
+++ tests/TipoDeEvento/tipoEvento.spec.js
@@ -1,19 +1,18 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
-const { timeout } = require('../../playwright.config');
-const { beforeEach } = require('node:test');
+import { test, expect } from '@playwright/test';
 
-
-test("test1", async ({ page }) => {
+test.beforeEach(async ({page}) => {
     await page.goto('http://infinitycode.tis.cs.umss.edu.bo');
     /** Aqui hacemos click en el menu lateral, para entrar en el menu **/
     const botonMenu = await page.getByRole('button', { name: 'TIPO DE EVENTO' })
-    await botonMenu.click();
+    await botonMenu.click(); 
+});
+
+test("test1", async ({ page }) => {
     const opcionCrearTipoEvento = await page.getByRole('link', { name: 'Crear tipo de evento' });
     await opcionCrearTipoEvento.click();
     const inputNombre = await page.getByPlaceholder('Ingrese el nombre del tipo de evento');
     await inputNombre.click();
-    await page.waitForTimeout(2500);
     await inputNombre.fill("Dorian lagarto");
     const inputDescripcion =  await page.getByPlaceholder('Ingrese una descripción...');
     await inputDescripcion.fill("Nueva descripcion")
